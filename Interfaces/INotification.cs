00457aa445f7bccb360e651d97f826fa4f71953c
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-08-07 16:32:30 +0700

    init

---
+++ Interfaces/INotification.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace dependencyinjection.Interfaces
+{
+    public interface INotification
+    {
+        void SendMessager(string message);
+    }
+}
---
+++ Services/ConsoleNotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace dependencyinjection.Services
+{
+    public class ConsoleNotification
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"ConsoleNotification.WriteMessage called. Message: {message}");
+        }
+    }
+
+
+}
