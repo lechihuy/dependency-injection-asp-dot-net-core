3394d9354dd33a567da6461d2eca99a45d84eba5
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-08-08 20:44:57 +0700

    wip

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using dependencyinjection.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dependencyinjection.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    private readonly ConsoleNotification _notification = new();
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        _notification.SendMessage("IndexModel.OnGet");
+    }
+}
